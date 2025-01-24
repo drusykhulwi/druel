c8f9dbbdd3271910a0601314a5c0c0fd4dd3189e
Drusilla Khulwael <drusillakhulwael@gmail.com>
2025-01-25 02:37:14 +0300

    upload image UI

--- frontend/src/pages/Upload.jsx
+++ frontend/src/pages/Upload.jsx
@@ -1,9 +1,46 @@
+import Navbar from "../components/Navbar"
+import { FaUpload } from 'react-icons/fa';
+import './Upload.css';
 
 
 function Upload() {
   return (
-    <div>
-      Upload
+    <div className="uploads">
+      <Navbar/>
+      <div className="uploads-container">
+        <h1>Upload Ultrasound Image</h1>
+        <div className="upload">
+          <div className="upload-container">
+            <div className="cont">
+              <div className="icons">
+                <FaUpload/>
+              </div>
+              <p>Drag and drop your ultrasound images here, or</p>
+              <button>Browse Files</button>
+              <p>Supported formats DICOM, JPG, PNG</p>
+            </div>
+          </div>
+          <form className="patient-details">
+            <div className="inputs">
+              <div className="input">
+                <label>Patient ID</label>
+                <input type="text" placeholder="Enter Patient ID" className="input-placeholder"/>
+              </div>
+              <div className="input">
+                <label>Scan Date</label>
+                <input type="date" placeholder="Enter Patient ID" className="input-date" />
+              </div>
+            </div>
+            <div className="notes-input">
+              <label>Notes</label>
+              <textarea type="text" placeholder="Add any additional notes..." className="input-placeholder"/>
+            </div>
+            <div className="upload-btn">
+              <button>Upload and Analyze</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
