904b8b600862cb1d71a3dbee013327a72971e441
Drusilla Khulwael <drusillakhulwael@gmail.com>
2025-04-12 14:08:31 +0300

    Backend update on routes

--- backend/App.js
+++ backend/App.js
@@ -1,4 +1,3 @@
 const analyzeRoutes = require("./routes/AnalyzeRoutes");
 app.use("/api", analyzeRoutes);
-const analyzeRoutes = require("./routes/analyze.routes");
-app.use("/api", analyzeRoutes);
+
--- backend/routes/AnalyzeRoutes.js
+++ backend/routes/AnalyzeRoutes.js
@@ -4,7 +4,12 @@ const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
 
 const { analyzeBrainScan } = require("../controllers/AnalyzeController");
+const { analyzeVentricular } = require("../controllers/AnalyzeVentricullarController");
+const { analyzeCerebellum } = require("../controllers/analyzeCerebellum.controller");
+
+router.post("/analyze-cerebellum", upload.single("image"), analyzeCerebellum);
 
 router.post("/analyze", upload.single("image"), analyzeBrainScan);
+router.post("/analyze-ventricular", upload.single("image"), analyzeVentricular);
 
 module.exports = router;
