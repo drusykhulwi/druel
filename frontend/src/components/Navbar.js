dcb19a55811cfa36b911bd77807e0c86c1861751
Drusilla Khulwael <drusillakhulwael@gmail.com>
2025-04-13 20:29:22 +0300

    Logout functionality

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,11 +1,25 @@
 // src/components/Navbar.js
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
+  };
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    const result = await logout();
+    if (result.success) {
+      navigate('/login');
+    } else {
+      alert(result.message);
+    }
   };
 
   return (
@@ -41,12 +55,12 @@ const Navbar = () => {
               </svg>
               Reports
             </a>
-            <a href="/login" className="ml-4 px-3 py-1 rounded hover:bg-druel-light-blue flex items-center">
+            <button onClick={handleLogout} className="ml-4 px-3 py-1 rounded hover:bg-druel-light-blue flex items-center">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
               </svg>
               Logout
-            </a>
+            </button>
           </div>
 
           {/* Hamburger Menu a (Mobile) */}
@@ -107,7 +121,7 @@ const Navbar = () => {
                 </svg>
                 Reports
               </a>
-              <a className="px-3 py-2 rounded hover:bg-druel-light-blue flex items-center">
+              <a href="/" className="px-3 py-2 rounded hover:bg-druel-light-blue flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
                 </svg>
