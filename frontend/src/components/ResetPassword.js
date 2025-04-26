00d73af01f676a1f109b2ea3217320bb42e70622
Drusilla Khulwael <drusillakhulwael@gmail.com>
2025-04-26 21:19:34 +0300

    Changed colour

--- frontend/src/components/ResetPassword.js
+++ frontend/src/components/ResetPassword.js
@@ -168,7 +168,7 @@ const ResetPassword = () => {
             <button
               type="submit"
               disabled={loading}
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:bg-indigo-400"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-druel-light-blue hover:bg-druel-blue focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:bg-indigo-400"
             >
               {loading ? 'Processing...' : 'Reset Password'}
             </button>
